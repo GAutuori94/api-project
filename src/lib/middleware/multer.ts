6e3b17cc5e338d68e29b8d0b1234854f94823063
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-09-22 18:04:21 +0200

    HTML form to upload files created in /web/upload.html; added route to API in /src/app.ts with relative test in server.test.ts; configured Multer to save file in disk in /src/lib/middleware/multer.ts

--- src/lib/middleware/multer.ts
+++ src/lib/middleware/multer.ts
@@ -1,7 +1,20 @@
 import multer from "multer";
+import { randomUUID } from "crypto";
+import mime from "mime";
+
+export const generatePhotoFilename = (mimeType: string) => {
+    const randomFilename = `${randomUUID()}-${Date.now()}`;
+    const fileExtension = mime.getExtension(mimeType);
+    const filename = `${randomFilename}.${fileExtension}`;
+
+    return filename
+}
 
 const storage = multer.diskStorage({
-    destination: "uploads/"
+    destination: "uploads/",
+    filename: (request, file, callback) => {
+        return callback(null, generatePhotoFilename(file.mimetype))
+    }
 })
 
 export const multerOptions = {};
